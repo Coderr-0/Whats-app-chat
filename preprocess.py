e3ad8f1ec47f548a0c945ddf5bdeb030acde9504
Coderr-0 <adityalahe10@gmail.com>
2026-03-30 04:15:38 +0530

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,66 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r'\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4},?\s\d{1,2}:\d{2}\s?(?:am|pm|AM|PM)?\s?-\s'
+
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    if len(messages) == 0:
+        raise ValueError("No valid WhatsApp messages found")
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    df['message_date'] = pd.to_datetime(df['message_date'], errors='coerce')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages_list = []
+
+    for message in df['user_message']:
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages_list.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages_list.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages_list
+    df.drop(columns=['user_message'], inplace=True)
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month_name()
+    df['month_num'] = df['date'].dt.month
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df['day_name'] = df['date'].dt.day_name()
+
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+    return df
